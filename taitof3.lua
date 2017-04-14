4ca102300e351fb6434efa6d8ba681a642face97
風見幽香 <PatheticFish@users.noreply.github.com>
2017-04-13 19:09:14 -0700

    fix/update f3 chip for personal use
    no idea what asciitable is
    inline'd notes.lua piano table because it didn't work the way I expected it to
    also extended the piano note table because it didn't seem to actually cover the whole display
    the emulator "sethook" seems to be outdated? idk, that's updated for new MAME
    commented out track title drawing because I didn't want to figure out the asciitable thing

--- taitof3.lua
+++ taitof3.lua
@@ -1,10 +1,10 @@
 --for i,v in pairs(manager:machine().devices) do print(i) end;
-snd = manager:machine().devices[":audiocpu"]
+snd = manager:machine().devices[":taito_en:audiocpu"]
 mem = snd.spaces["program"]
 gui = manager:machine().screens[":screen"]
 
-dofile("notes.lua")--musical notes
-dofile("asciitable.lua")--hex2text
+--dofile("notes.lua")--musical notes
+--dofile("asciitable.lua")--hex2text
 
 gtranspose = -23
 
@@ -13,6 +13,184 @@ Addresses = {
 0x58EE,0x587E,0x5846,
 0x580E,0x57D6,0x579E,0x5766,
 0x5D4E
+
+}
+playkx = {
+  0, --C   1
+  2, --C#  2
+  4, --D   3
+  6, --D#  5
+  8, --E   5
+ 12, --F   6
+ 14, --F#  7
+ 16, --G   8
+ 18, --G#  9
+ 20, --A  10
+ 22, --A# 11
+ 24, --B  12
+ 28, --C  13
+ 30, --C# 14
+ 32, --D  15
+ 34, --D# 16
+ 36, --E  17
+ 40, --F  18
+ 42, --F# 19
+ 44, --G  20
+ 46, --G# 21
+ 48, --A  22
+ 50, --A# 23
+ 52, --B  24
+ 56, --C  25
+ 58, --C# 26
+ 60, --D  27
+ 62, --D# 28
+ 64, --E  29
+ 68, --F  30
+ 70, --F# 31
+ 72, --G  32
+ 74, --G# 33
+ 76, --A  34
+ 78, --A# 35
+ 80, --B  36
+ 84, --C  37
+ 86, --C# 38
+ 88, --D  39
+ 90, --D# 40
+ 92, --E  41
+ 96, --F  42
+ 98, --F# 43
+100, --G  44
+102, --G# 45
+104, --A  46
+106, --A# 47
+108, --B  48
+112, --C  49
+114, --C# 50
+116, --D  51
+118, --D# 52
+120, --E  53
+124, --F  54
+126, --F# 55
+128, --G  56
+130, --G# 57
+132, --A  58
+134, --A# 59
+136, --B  60 
+140, --C  61
+142, --C# 62
+144, --D  63
+146, --D# 64
+148, --E  65
+152, --F  66
+154, --F# 67
+156, --G  68
+158, --G# 69
+160, --A  70
+162, --A# 71
+164, --B  72
+168, --C  72
+170, --C# 73
+172, --D  74
+174, --D# 75
+176, --E  76
+180, --F  77
+182, --F# 78
+184, --G  79
+186, --G# 80
+188, --A  81
+190, --A# 82
+192, --B  83
+196, --C  84
+
+}
+--84
+
+playky = {
+  4, --C   1
+  1, --C#  2
+  4, --D   3
+  1, --D#  4
+  4, --E   5
+  4, --F   6
+  1, --F#  7
+  4, --G   8
+  1, --G#  9
+  4, --A  10
+  1, --A# 11
+  4, --B  12
+  4, --C  13
+  1, --C# 14
+  4, --D  15
+  1, --D# 16
+  4, --E  17
+  4, --F  18
+  1, --F# 19
+  4, --G  20
+  1, --G# 21
+  4, --A  22
+  1, --A# 23
+  4, --B  24
+  4, --C  25
+  1, --C# 26
+  4, --D  27
+  1, --D# 28
+  4, --E  29
+  4, --F  30
+  1, --F# 31
+  4, --G  32
+  1, --G# 33
+  4, --A  34
+  1, --A# 35
+  4, --B  36
+  4, --C  37
+  1, --C# 38
+  4, --D  39
+  1, --D# 40
+  4, --E  41
+  4, --F  42
+  1, --F# 43
+  4, --G  44
+  1, --G# 45
+  4, --A  46
+  1, --A# 47
+  4, --B  48
+  4, --C  49
+  1, --C# 50
+  4, --D  51
+  1, --D# 52
+  4, --E  53
+  4, --F  54
+  1, --F# 55
+  4, --G  56
+  1, --G# 57
+  4, --A  58
+  1, --A# 59
+  4, --B  60
+  4, --C  61
+  1, --C# 62
+  4, --D  63
+  1, --D# 64
+  4, --E  65
+  4, --F  66
+  1, --F# 67
+  4, --G  68
+  1, --G# 69
+  4, --A  70
+  1, --A# 71
+  4, --B  72
+  4, --C  73
+  1, --C# 74
+  4, --D  75
+  1, --D# 76
+  4, --E  77
+  4, --F  78
+  1, --F# 79
+  4, --G  80
+  1, --G# 81
+  4, --A  82
+  1, --A# 83
+  4, --B  84
+  4, --C  85
 
 }
 
@@ -25,6 +203,7 @@ Notes
 ]]
 
 function main()
+--print("main")
 measure = mem:read_u16(0xFFD0B0)
 beat = mem:read_u16(0xFFD0B2)+1
 tempo = mem:read_u16(0xFFD0E6)
@@ -54,9 +233,9 @@ function ascii(addr,length,x,y)
 gui:draw_box(x,y,x+128,y+10,0xC0EE00DD,0xFFFFFFFF)
 for text = 0,length,1 do
 	addr = addr + 1
-	textp1 = mem:read_u8(addr)
-	ftext = asciitable[textp1+1]
-	gui:draw_text(2+x+text*8,y,ftext)
+--	textp1 = mem:read_u8(addr)
+--	ftext = asciitable[textp1+1]
+--	gui:draw_text(2+x+text*8,y,ftext)
 	end
 end
 
@@ -107,6 +286,7 @@ return val
 end
 
 function drawplaynote(mnote,x,y,color1,color2)
+print(mnote)
 x1 = x + playkx[mnote]
 y1 = y + playky[mnote]
 x2 = 3 + x1
@@ -116,4 +296,5 @@ gui:draw_box(x1,y1,x2,y2,color1,color2)
 
 end
 
-emu.sethook(main,"frame")
+--emu.sethook(main,"frame")
+emu.register_frame_done(main)
